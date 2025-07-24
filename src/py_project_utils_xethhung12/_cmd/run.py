11a8da15d00a87da70cdceb5ab2eab1467148e12
Ubuntu <ubuntu@ip-172-31-28-6.us-west-2.compute.internal>
2025-07-24 02:57:32 +0000

    add jvault_http_client

--- src/py_project_utils_xethhung12/_cmd/run.py
+++ src/py_project_utils_xethhung12/_cmd/run.py
@@ -4,8 +4,9 @@ import os
 import importlib.resources as resources
 import py_project_utils_xethhung12 as py_project
 import argparse
+from j_vault_http_client_xethhung12 import jvault_http_client
 def main():
-    # py_project.create_project(sys.argv[1],sys.argv[2:])
+    jvault_http_client.load_to_env()
     parser = argparse.ArgumentParser(
         prog = "PyProjectUtils",
         description="The PyProjectUtils is a tools to help create and manage py project. (Consider it as a automation to create python packaging tool)",
--- src/py_project_utils_xethhung12/data/src$___project_name___$_cmd$run.py
+++ src/py_project_utils_xethhung12/data/src$___project_name___$_cmd$run.py
@@ -1,3 +1,5 @@
 import ___project_name___ as project
+from j_vault_http_client_xethhung12 import jvault_http_client
 def main():
+    jvault_http_client.load_to_env()
     project.hello("user")
