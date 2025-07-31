63e552195de85589d341af5693920b5239017e39
Ubuntu <ubuntu@ip-172-31-28-6.us-west-2.compute.internal>
2025-07-31 20:41:23 +0800

    update

--- src/py_project_utils_xethhung12/data/src$___project_name___$__main__.py
+++ src/py_project_utils_xethhung12/data/src$___project_name___$__main__.py
@@ -1,3 +1,9 @@
+import os
+if os.getenv("DEV") is not None:
+    import sys
+    p=os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    sys.path.insert(0, p)
+
 from ___project_name___._cmd import run
 if __name__ == "__main__":
     run.main()
